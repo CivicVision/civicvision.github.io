3fd6161ce09ea0fd1363b49bb4b6d040018ab075
Mila Frerichs <mila.frerichs@gmail.com>
2015-06-03 15:40:56 +0200

    Add middleman blog

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 
 gem "middleman", "~>3.3.10"
 gem "middleman-deploy"
+gem "middleman-blog"
+gem "nokogiri"
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
--- config.rb
+++ config.rb
@@ -1,4 +1,31 @@
 activate :i18n, :mount_at_root => :de
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+  blog.layout = "article"
+
+  blog.permalink = "{lang}/{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "{lang}/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.summary_separator = /(READMORE)/
+  blog.summary_length = 150
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -41,6 +68,14 @@ helpers do
   def locale_active(locale)
     I18n.locale == locale.to_sym ? { class: "active" } : {}
   end
+
+  def locale_dateformat
+    case I18n.locale
+    when :de then '%e. %b %Y'
+    when :en then '%b %e %Y'
+    end
+  end
+
 #   def some_helper
 #     "Helping"
 #   end
