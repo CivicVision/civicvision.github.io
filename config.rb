a3b3f6b4e694ed63a500cbafb8c7b1093a804393
milafrerichs <mila.frerichs@gmail.com>
2016-06-15 12:20:54 +0200

    English is the default and has no prefix.
    the focus is on english and german will have a prefix. old sites will be
    english by default which will not be a huge deal

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-activate :i18n, mount_at_root: false
+activate :i18n, mount_at_root: :en
 
 activate :syntax
 set :markdown_engine, :redcarpet
@@ -30,40 +30,11 @@ activate :blog do |blog|
 end
 
 page "/feed.xml", layout: false
-###
-# Page options, layouts, aliases and proxies
-###
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
 helpers do
   def nav_active(path)
     current_page.path == path ? { class: "active "} : {}
@@ -93,9 +64,6 @@ helpers do
     date.strftime(locale_dateformat)
   end
 
-#   def some_helper
-#     "Helping"
-#   end
 end
 
 set :css_dir, 'stylesheets'
@@ -111,35 +79,12 @@ end
 ignore "/de/book/pay.html"
 ignore "/en/book/pay.html"
 
-redirect "index.html", to: "en/index.html"
-redirect "blog/index.html", to: "de/blog/index.html"
-redirect "blog/index.html", to: "de/blog/index.html"
-redirect "legal/index.html", to: "/de/legal/index.html"
-redirect "legal/privacy/index.html", to: "/de/legal/privacy/index.html"
-redirect "contact/index.html", to: "/de/contact/index.html"
-redirect "contact/thank-you/index.html", to: "/de/contact/thank-you/index.html"
-redirect "newsletter/index.html", to: "/de/newsletter/index.html"
-redirect "newsletter/thank-you/index.html", to: "/de/newsletter/thank-you/index.html"
-redirect "book/index.html", to: "/de/book/index.html"
 
-# Build-specific configuration
 configure :build do
   ignore '*.swp'
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
   activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
+
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
