dac05c1398172e1569d43e9b7375d8ccf827dc10
milafrerichs <mila.frerichs@gmail.com>
2016-12-27 15:07:16 +0100

    Add js into seperate file

---
+++ source/javascripts/cta-button.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("#free-call-modal").on("change", function() {
+    //Record Drip event
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
