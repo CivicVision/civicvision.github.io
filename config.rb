7dc6fc2b2d50d053272f9c0248dad0203812a207
milafrerichs <mila.frerichs@gmail.com>
2017-07-28 11:39:05 +0200

    Add main page for data-stories, update slug for all of them

--- config.rb
+++ config.rb
@@ -51,8 +51,8 @@ activate :blog do |blog|
   blog.prefix = "data-stories"
   blog.name = "data-stories"
   blog.layout = "ds-article"
-  blog.permalink = "{title}.html"
-  blog.sources = "{title}.html"
+  blog.permalink = "{category}-{title}.html"
+  blog.sources = "{category}-{title}.html"
 end
 
 page "/feed.xml", layout: false
